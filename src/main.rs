309629b863cd715aff3267d3787e24ca6f7ecb65
Nicolas Peralta <nperaltapz@gmail.com>
2024-08-19 15:49:21 -0400

    use of scopes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+    HttpResponse::Ok().body("<h1>Hello World!</h1>")
 }
 
 #[post("/echo")]
@@ -18,10 +17,12 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("hey", web::get().to(manual_hello))
+        App::new().service(
+            web::scope("/app")
+                .service(hello)
+                .service(echo)
+                .route("hey", web::get().to(manual_hello)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
