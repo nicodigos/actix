3c979daaab24c5d1123182c41b883f8bb847476b
Nicolas Peralta <nperaltapz@gmail.com>
2024-08-20 21:57:04 -0400

    restarted on my own

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,20 @@
+use core::panic;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-struct AppState {
-    name: String,
-    any_number: i32,
-}
-
-#[get("/app-state")]
-async fn display_app_state(data: web::Data<AppState>) -> impl Responder {
-    let name = &data.name;
-    let number = &data.any_number;
-    HttpResponse::Ok().body(format!("<h1>AppState: {}, {}</h1>", name, number))
-    
-}
-
-struct AnotherState {
-    another_name: String
-}
-
-#[get("/another-state")]
-async fn display_another_state(data: web::Data<AnotherState>) -> impl Responder {
-    let name = &data.another_name;
-    HttpResponse::Ok().body(format!("<h1>Another State: {}</h1>", name))
-}
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("<h1>Hello World!</h1>")
+    HttpResponse::Ok().body("Hello World")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("<h1>Hey there!</h1>")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .app_data(web::Data::new(
-            AppState{name: String::from("MyApp"), any_number: 10}
-        )) 
-        .app_data(web::Data::new(
-            AnotherState{another_name:String::from("Example")}
-        ))       
-        .service(
-            web::scope("/app")
-                .service(hello)
-                .service(echo)
-                .route("hey", web::get().to(manual_hello)),
-        )
-        .service(
-            web::scope("trying-state")
-            .service(display_app_state)
-            .service(display_another_state)
-        )
-    })
+    HttpServer::new(|| App::new()
+                        .service(hello)
+                    )   
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
